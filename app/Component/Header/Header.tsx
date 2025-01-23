4c0bcf4214421659723936842f6f46b6b6f8ae2d
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2025-01-23 18:40:22 +0600

    added header and footer0

---
+++ app/Component/Header/Header.tsx
@@ -0,0 +1,147 @@
+"use client"
+
+import React, { useEffect, useState } from "react"
+import logo from "@/public/images/logo.png"
+import { Button, Divider, Dropdown, Flex, MenuProps, Tooltip } from "antd"
+import {
+  GoogleOutlined,
+  HeartOutlined,
+  MessageOutlined,
+} from "@ant-design/icons"
+
+import { usePathname, useRouter } from "next/navigation"
+import Link from "next/link"
+// import {
+//   ClientSafeProvider,
+//   getProviders,
+//   LiteralUnion,
+//   signIn,
+//   signOut,
+//   useSession,
+// } from "next-auth/react"
+import Image from "next/image"
+import SearchProperties from "../SearchProperties"
+
+export default function Header() {
+  const pathname = usePathname()
+  const router = useRouter()
+//   const { data: session } = useSession()
+//   const [providers, setProviders] = useState<Record<
+//     LiteralUnion<string>,
+//     ClientSafeProvider
+//   > | null>(null)
+
+//   console.log(session)
+
+//   useEffect(() => {
+//     const setAuthProviders = async () => {
+//       const res = await getProviders()
+//       setProviders(res)
+//     }
+//     setAuthProviders()
+//   }, [session])
+
+  const items: MenuProps["items"] = [
+    {
+      key: "profile",
+      label: "Profile",
+      onClick: () => {
+        router.push("profile")
+      },
+    },
+    {
+      key: "saved-properties",
+      label: "Saved Properties",
+      onClick: () => {
+        router.push("saved-properties")
+      },
+    },
+    {
+      key: "logout",
+      label: "Logout",
+      danger: true,
+    //   onClick: () => {
+    //     signOut()
+    //   },
+    },
+  ]
+
+  return (
+    <nav className="nav">
+      <div className="flex-between">
+        <div
+          className="flex-align-center"
+          style={{
+            gap: 16,
+          }}
+        >
+          <Link href={"/"}>
+            <img src={logo.src} alt="logo" width={200} className="pointer" />
+          </Link>
+          <Link href={"/properties"}>
+            <Button ghost>Properties</Button>
+          </Link>
+        </div>
+
+        {/* {!session && providers && ( */}
+            
+          <Button
+            icon={<GoogleOutlined />}
+            // onClick={() => signIn(providers.google.id)}
+          >
+            Login or Register
+          </Button>
+        {/* )} */}
+        {/* {session && ( */}
+
+          <Flex align="center" gap={22}>
+            <Tooltip title="Messages">
+              <MessageOutlined
+                className="header-icons"
+                onClick={() => router.push("/messages")}
+              />
+            </Tooltip>
+            <Tooltip title="Saved Properties">
+              <HeartOutlined
+                className="header-icons"
+                onClick={() => router.push("/saved-properties")}
+              />
+            </Tooltip>
+            <Dropdown menu={{ items }} trigger={["click"]}>
+              {/* <Image
+                src={session?.user?.image as string}
+                width={40}
+                height={40}
+                alt={session?.user?.name || "user"}
+                className="profile-img"
+              /> */}
+            </Dropdown>
+          </Flex>
+        {/* )} */}
+      </div>
+      {(pathname === "/" || pathname.startsWith("/search-results")) && (
+        <>
+          <hr />
+          <div className="hero">
+            <h1 className="hero-title">
+              Finding your <span className="color-sec">perfect home</span>{" "}
+            </h1>
+            <p className="hero-subtitle">
+              Search for your perfect home in the best locations around the
+              world
+            </p>
+            <SearchProperties />
+            <Flex justify="center" gap={8}>
+              <Link href={"/properties"}>
+                <Button>Browse Properties</Button>
+              </Link>
+              <Link href={"/properties/add"}>
+                <Button>List Property</Button>
+              </Link>
+            </Flex>
+          </div>
+        </>
+      )}
+    </nav>
+  )
+}
---
+++ app/Component/SearchProperties.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+// import { useMessage } from "@/context/MessageContext"
+import { Form, Input, Select, Space } from "antd"
+import { useRouter } from "next/navigation"
+import React, { useState } from "react"
+
+export default function SearchProperties() {
+  const [location, setLocation] = useState("")
+  const [type, setType] = useState("RENT")
+//   const { showMessage } = useMessage()
+  const router = useRouter()
+
+//   const handleGetProperties = () => {
+//     if (location.trim() === "") {
+//       return showMessage("Please enter a location", "error")
+//     } else {
+//       const queryString = `?location=${location}&propertyType=${type}`
+//       router.push("/search-results" + queryString)
+//     }
+//   }
+
+  return (
+    <div className="flex-center px-1">
+      <Form 
+    //   onFinish={handleGetProperties}
+      >
+        <Space.Compact>
+          <Form.Item name="type">
+            <Select
+              defaultValue={"RENT"}
+              options={[
+                {
+                  value: "RENT",
+                  label: "For Rent",
+                },
+                {
+                  value: "SALE",
+                  label: "For Sale",
+                },
+              ]}
+              style={{ width: 120 }}
+              size="large"
+              value={type}
+              onChange={(val) => setType(val)}
+            />
+          </Form.Item>
+          <Form.Item name={"location"}>
+            <Input.Search
+              size="large"
+              enterButton="Search"
+              placeholder="Search for a location..."
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            //   onSearch={handleGetProperties}
+            />
+          </Form.Item>
+        </Space.Compact>
+      </Form>
+    </div>
+  )
+}
